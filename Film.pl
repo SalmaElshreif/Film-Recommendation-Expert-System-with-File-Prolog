2087fa514fd9b0665bd8534facad5e50821b925d
Salma Elshreif <salma.elshreif2001@gmail.com>
2022-06-04 00:09:22 +0200

    Add files via upload

---
+++ Film.pl
@@ -0,0 +1,156 @@
+/** new */                                    %90 short
+film('El-Badla', 'Mohamed El-adl', 'new', 'drama', 'others', 89, 'egypt', 'Tamer Hosny', 7.6, 12).
+film('El-Mamar', 'Sherif Arafa', 'new', 'drama', 'others', 150, 'egypt', 'Ahmed Ezz', 7.9, 30).
+film('A.I. Tales', 'Kristen Hilkert', 'new', 'drama', 'others',83, 'german', 'Pom Klementieff', 7.9, 520).
+film('Pride & Prejudice','Joe Wright','new', 'drama', 'others', 129, 'england', ' Keira Knightley', 7.8, 248).
+
+film('Amir El-Behar', 'Wael Ehsan', 'new', 'comedy', 'others', 110, 'egypt', 'Mohamed Henedy', 7.3, 537).
+film('Dido', 'Amr Salah','new', 'comedy', 'others', 90, 'egypt', 'Karim Fahmy', 8.7, 393).
+film('The Devil Wears Prada', 'David Frankel', 'new', 'comedy', 'others', 109, 'usa', 'Meryl Streep', 6.9, 227).
+film('Relish', 'Justin Ward ', 'new', 'comedy', 'others', 89, 'usa', 'Hana Hayes', 7.9, 227).
+
+film('Sherlock Holmes', 'Guy Ritchie', 'new', 'adventure','family', 128, 'england', 'Robert Downey Jr', 7.8, 317).
+film('El-Kanz: El-Haqiqah Wa El-Khayal', 'Sharif Arafah', 'new', 'adventure', 'others', 157, 'egypt', 'muhammad saad', 6.9, 295).
+film('Pirates Of The Caribbean: The Curse Of The Black Pearl', 'Gore Verbinski', 'new', 'adventure', 'others', 89, 'usa', 'Johnny Depp',8, 129).
+film('Nady El-Regal El-Serry','Khaled El Halafawy','new','adventure','others', 87, 'egypt', 'karim abdel aziz',8.5, 126).
+
+
+film('Encanto', 'Byron Howard', 'new', 'animation', 'others', 102, 'usa', 'Stephanie Beatriz', 7.2, 290).
+film('Up', 'Pete Docter', 'new', 'animation', 'others', 89, 'usa', 'Edward Asner', 7.4, 161).
+film('Finding Nemo', 'Andrew Stanton', 'new', 'animation', 'others', 100, 'egypt', 'Abla kamel', 7.1, 126).
+film('Minions', 'Kyle Balda', 'new', 'animation', 'others', 81, 'egypt','Scarlet Overkill', 6.4, 59).
+
+film('In Good Hands', 'Ketche', 'new', 'romance', 'others', 105, 'turkey', 'Asli Enver', 7.5, 164).
+film('Kesset Hob', 'Osman Abo-Laban', 'new', 'romance', 'others', 85, 'egypt', 'Hanah El-zahed', 7.6, 100).
+film('500 Days Of Summer', 'Marc Webb', 'new', 'romance', 'others', 80, 'usa', 'Joseph Gordon-Levitt', 7.2, 99).
+film('Mesh Ana', 'Sarah Wafiq', 'new', 'romance', 'others', 98, 'egypt', 'Tamer Hosny', 7.0, 102).
+
+film('I Am Legend', 'Francis Lawrence', 'new', 'horror', 'monsters', 81, 'australia', 'Will Smith', 7.5, 344).
+film('A Tale Of Two Sisters', 'Jee-woon Kim', 'new', 'horror', 'others', 115,'korean', 'Kap-su Kim', 6.6, 158).
+film('El-Feal El-Azrak', 'Marawan Hamed', 'new', 'horror', 'ghosts', 129, 'egypt', 'karim Abdel Aziz', 7.4, 116).
+film('El-Hareth', 'Mohammed Nader', 'new', 'horror', 'ghosts', 84, 'egypt', 'Ahmed EL Fishawi', 6.8, 84).
+
+
+
+/** old */
+film('Sahar El-Layaly', 'Hany Khalefa', 'old', 'drama', 'others', 82, 'egypt', 'Hanan Tork', 8.1, 552).
+film('Zaman Ya Hob', 'Atef Salem', 'old', 'drama', 'others', 122, 'egypt', 'Farid El-atrash', 8.3, 410).
+film('The Village', 'M. Night Shyamalan', 'old', 'drama', 'others', 86, 'usa', 'Sigourney Weaver', 8.3, 410).
+film('The Orphanage', 'J.A. Bayona', 'old', 'drama', 'others', 125, 'usa', 'Belen Rueda', 8.3, 410).
+
+film('Esabet Hamada W Toto', 'Mohamed Abd El-aziz', 'old', 'comedy', 'others', 110, 'egypt', 'Adel Emam', 8.6, 697).
+film('Home Alone', 'Chris Columbus','old', 'comedy', 'others', 89, 'usa', 'Macaulay Culkin', 7.1, 297).
+film('El-Walad El-Ghaby', 'Madkour Thabet','old', 'comedy', 'others', 75, 'egypt', 'Hala Fakher', 7.8, 261).
+film('3 Idiots', 'Rajkumar Hirani','old', 'comedy', 'others', 192, 'india', 'Aamir Khan', 7.8, 261).
+
+film('Ghazl El-Banat', 'Anwar Wagdi', 'old', 'adventure','Musical', 88, 'egypt', 'Nagib El-Rihani', 7.5, 269).
+film('Ayam El-Sadat', 'Mohamed Khan', 'old', 'adventure', 'history',165, 'egypt', 'Ahmed Zaki', 9, 269).
+film('The Mummy', 'Stephen Sommers', 'old', 'adventure', 'fantasy',124, 'usa', ' Brendan Fraser', 8.9, 269).
+film('Jumanji', 'Joe Johnston', 'old', 'adventure', 'fantasy',84,'usa',' Robin Williams', 9, 189).
+
+
+film('Shrek', 'Andrew Adamson', 'old', 'animation', 'others', 114, 'usa', 'Mike Myers', 7.9, 604).
+film('The Lion King', 'Jon Favreau', 'old', 'animation', 'others', 109, 'egypt', 'Safaa El-Toukhi', 9.3, 381).
+film('Toy Story', 'John Lasseter', 'old', 'animation', 'others', 80, 'usa', 'Tom Hanks', 7.4, 124).
+film('Tarzan', 'Chris Buck', 'old', 'animation', 'others', 88, 'egypt', 'Maged Elgedwany', 6.4, 42).
+
+film('Amelia', 'Jean-Pierre Jeunet', 'old', 'romance', 'others', 77, 'france', 'Audrey Tautou', 7.6, 203).
+film('Dangerous Beauty', 'Marshall Herskovitz', 'old', 'romance', 'others', 170, 'usa', 'Catherine McCormack', 6.2, 129).
+film('Qalb Gary2', 'Mohammed Elnaggar', 'old', 'romance', 'others', 103, 'egypt', 'Mustafa Amer', 7.0, 102).
+film('Khaly Balaak Mn Zouzou', 'Hassan ELemam', 'old', 'romance', 'others', 83, 'egypt', 'Soaad Hosny', 7.0, 102).
+
+film('The Shining', 'Stanley Kubrick', 'old', 'horror', 'psychopaths', 146, 'usa', 'Jack Nicholson', 7.9, 293).
+film('Dark Cloud', 'Jay Ness', 'old', 'horror', 'psychopaths', 81, 'usa', 'Emily Atack', 6.7, 172).
+film('Camp', 'Abed El Aziz Hashad', 'old', 'horror', 'others', 73, 'egypt', 'Amira Hany', 7.3, 148).
+film('Shams El-Zanaty', 'Samir Seif', 'old', 'horror', 'others', 146, 'egypt', 'Adel Emam', 7.3, 148).
+
+
+
+
+year(Y,E) :- film(Y,_,X,_,_,_,_,_,_,_),   X =='old',E =='older'.
+year(Y,E) :- film(Y,_,X,_,_,_,_,_,_,_),   X == 'new',E =='newer'.
+duration(Y,E):- film(Y,_,_,_,_,X,_,_,_,_), 20 =<X , X=< 90, E =='short'.
+duration(Y,E) :- film(Y,_,_,_,_,X,_,_,_,_), 91 =< X, E =='long'.
+egyptornot(Y,E) :- film(Y,_,_,_,_,_,X,_,_,_),X = 'egypt',E =='egypt'.
+egyptornot(Y,E) :- film(Y,_,_,_,_,_,X,_,_,_),X \='egypt',E =='foreign'.
+
+
+/*   01////OLD OR NEW   02///// TYPE    03/////DURATION  */
+
+
+year(Y,E1,E2,E3,E4):- test(Y,E1,E2,E3,E4).
+
+/*film('Ayam El-Sadat', 'Mohamed Khan', 'old', 'adventure', 'history',165, 'Egypt', 'Ahmed Zaki', 9, 269).*/
+
+test(Y,E1,E2,E3,E4) :-  tell('Film Recommendation.txt'),write('Hello in our Film Recommendation System :)'),nl, year(Y,E1),film(Y,_,_,E2,_,_,_,_,_,_),duration(Y,E3),egyptornot(Y,E4),nl,write('You should watch this movie:'),nl,writeinf(Y),nl.
+writeinf(Y):- nl,write(Y),told.
+
+recommendation(X, Y, E)     :- (X == 'woman', Y == 'happy'),    E = 'comedy, adventure, romance,thriller'.
+recommendation(X, Y, E)     :- (X == 'woman', Y == 'sad'),      E = 'drama, animation'.
+recommendation(X, Y, E)     :- (X == 'woman', Y == 'normal'),   E = 'horror, animation, romance'.
+recommendation(X, Y, E)     :- (X == 'man',   Y == 'happy'),    E = 'comedy, adventure,romance ,thriller'.
+recommendation(X, Y, E)     :- (X == 'man',   Y == 'sad'),      E = 'drama, comedy'.
+recommendation(X, Y, E)     :- (X == 'man',   Y == 'normal'),   E = 'horror, animation, thriller'.
+recommendation(X, Y, E).
+
+
+choose:-
+        write('     Film Recommendtation system  by Mariam Hosny & Salma Elshreif :)   '),nl,
+        write('     Please to help us try to answer the following questions below    '),nl,
+        write('     It helps to find best film for you    '),nl,nl,
+                
+
+        write('   What is your name? '),
+        read(Name),nl,
+        write('   Hello '),
+        write(Name),write(' :)'),nl,nl,
+        
+        write('   What is your sex? '),nl,
+        write('1. Woman'),nl,
+        write('2. Man'),nl,
+        read(Sex),nl,
+        
+        write('   What is your mood? '),nl,
+        write('1. Sad'),nl,
+        write('2. Happy'),nl,
+        write('3. Normal'),nl,
+        read(Mood),nl,
+        
+        write('Thanks to your answers we recommend following film gendre:'),nl,
+        recommendation(Sex, Mood, Gendre),
+        write(Gendre),nl,nl,
+
+
+        write('   Do you want old or new films?'),nl,
+        write('1. Old'),nl,
+        write('2. New'),nl,
+        read(A1),nl,
+        (A1 == 'old' ->Ans1 ='older';Ans1 ='newer'),
+        
+
+        write('   What film gendre do you prefer?'),nl,
+        write('1. Animation'),nl,
+        write('2. Drama'),nl,
+        write('3. Horror'),nl,
+        write('4. Comedy'),nl,
+        write('5. Romance'),nl,
+        write('6. Adventure'),nl,
+        read(Ans2),nl,
+
+
+        write('   How many time do you have?'),nl,
+        write('1. Little'),nl,
+        write('2. Many'),nl,
+        read(C1),nl,
+        (C1 == 'little' ->Ans3 ='short';Ans3 ='long'),
+        
+        write('   Do you want an egyptian film?'),nl,
+        write('1. Yes'),nl,
+        write('2. No'),nl,
+        read(D1),nl,
+        (D1 == 'no' -> Ans4 ='foreign';Ans4 ='egypt'),
+        
+        year(Y,Ans1,Ans2,Ans3,Ans4),
+        write('You should watch this movie:'),nl,nl,
+        write(Y),nl,nl,
+        write('We suggest that you make popcorn and drink juice with the movie.'),nl.
